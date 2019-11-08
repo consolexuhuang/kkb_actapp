4c8e4f81f36b54b8abc1b14a4ebae2fc3ca43111
consolexuhuang <1758050394@qq.com>
2019-11-08 18:46:49 +0800

    更新

--- app.js
+++ app.js
@@ -15,17 +15,18 @@ App({
         // 请求完新版本信息的回调
         if (res.hasUpdate) {
           updateManager.onUpdateReady(function () {
-            wx.showModal({
-              title: '更新提示',
-              content: '新版本已经准备好，是否重启应用？',
-              showCancel: false,
-              success: function (res) {
-                if (res.confirm) {
-                  // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-                  updateManager.applyUpdate()
-                }
-              }
-            })
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+            // wx.showModal({
+            //   title: '更新提示',
+            //   content: '新版本已经准备好，是否重启应用？',
+            //   showCancel: false,
+            //   success: function (res) {
+            //     if (res.confirm) {
+                  
+            //     }
+            //   }
+            // })
           })
           updateManager.onUpdateFailed(function () {
             // 新的版本下载失败
